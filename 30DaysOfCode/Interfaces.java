5dde060b2b635c40c3962befa81dd6c6ced8c8b6
BytePair <bytepair@gmail.com>
2017-05-31 02:43:21 -0400

    Add Interfaces.java

---
+++ 30DaysOfCode/Interfaces.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.util.*;
+
+
+interface AdvancedArithmetic{
+   int divisorSum(int n);
+}
+
+
+class Calculator implements AdvancedArithmetic {
+    
+    public int divisorSum(int n) {
+        int tempSum = 0;
+        for (int i = n; i >= 1; i--) {
+            if (n % i == 0) {
+                tempSum += i;
+            }
+        }
+        return tempSum;
+    }
+}
+
+
+class Interfaces {
+
+    public static void main(String[] args) {
+    	
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        scan.close();
+        
+      	AdvancedArithmetic myCalculator = new Calculator(); 
+        int sum = myCalculator.divisorSum(n);
+        System.out.println("I implemented: " + myCalculator.getClass().getInterfaces()[0].getName() );
+        System.out.println(sum);
+    }
+
+}
