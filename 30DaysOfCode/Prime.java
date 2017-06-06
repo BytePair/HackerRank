8783ab464058c56c987fccb880b0135b4da7158c
BytePair <bytepair@gmail.com>
2017-06-06 00:05:45 -0400

    Better primes

--- 30DaysOfCode/Prime.java
+++ 30DaysOfCode/Prime.java
@@ -6,34 +6,27 @@ public class Solution {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         int count = s.nextInt();
-        int[] numbers = new int[count];
+        long[] numbers = new long[count];
         for (int i = 0; i < count; i++) {
-            numbers[i] = s.nextInt();
-        }
-        for (int i = 0; i < count; i++) {
-            int current_number = numbers[i];
-            // is definitely prime
-            if (current_number == 1 || current_number == 2) {
+            numbers[i] = s.nextLong();
+            if (isPrime(numbers[i])) {
                 System.out.println("Prime");
-            } 
-            // check for prime
+            }
             else {
-                boolean is_prime = true;
-                for(int j = 1; j < current_number; j++) {
-                    if (j != 1 && j != current_number) {
-                        if (current_number % j == 0) {
-                            is_prime = false;
-                            break;
-                        }
-                    }
-                }
-                if (is_prime) {
-                    System.out.println("Prime");
-                }
-                else {
-                    System.out.println("Not prime");
-                }
+                System.out.println("Not prime");
             }
         }
+    }
+        
+    public static boolean isPrime(long a) {
+        if (a == 1 || a == 2) {
+            return false;
+        }
+        for (int i = 2; i < a; i++) {
+            if (a % i == 0) {
+                return false;
+            }
+        }
+        return true;
     }
 }
