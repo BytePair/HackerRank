22cf4436b71c722013bf941a0b0f788174b42d21
GH <bytepair@gmail.com>
2017-06-07 20:25:28 -0400

    Add day 27

---
+++ 30DaysOfCode/Testing.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+
+public class Testing {
+
+    public static void main(String[] args) {
+    
+        System.out.println("5");
+        System.out.println("3 3");
+        System.out.println("-1 0 8");
+        System.out.println("4 2");
+        System.out.println("-1 0 5 8");
+        System.out.println("5 4");
+        System.out.println("-1 -2 0 5 12");
+        System.out.println("6 1");
+        System.out.println("-1 2 0 8 -2 5");
+        System.out.println("7 7");
+        System.out.println("-1 -2 2 8 12 4 20");      
+
+    }
+    
+}
