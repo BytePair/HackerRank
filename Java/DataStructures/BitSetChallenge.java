1312a64332e194d50b0c9e0c32a6ceedd6d8950e
Gerald Holmes <bytepair@gmail.com>
2017-07-09 00:10:42 -0400

    Finish BitSet Challenge

--- Java/DataStructures/BitSetChallenge.java
+++ Java/DataStructures/BitSetChallenge.java
@@ -17,64 +17,48 @@ public class BitSetChallenge {
         BitSet second = new BitSet();
 
         for (int k = 0; k < numCommands; k++) {
+
             String nextCommand = s.next();
             int firstArg = s.nextInt();
             int secondArg = s.nextInt();
-            // sloppy if/else incoming...
-            if (nextCommand.equals("AND")) {
-                if (firstArg == 1) {
-                    first.and(second);
-                } 
-                else {
-                    second.and(first);
-                }
+            
+            // use switch instead of chained if/else statements
+            switch (nextCommand) {
+
+                case "AND":
+                    if (firstArg == 1) first.and(second);
+                    else second.and(first);
+                    break;
+
+                case "OR":
+                    if (firstArg == 1) first.or(second);
+                    else second.or(first);
+                    break;
+
+                case "XOR":
+                    if (firstArg == 1) first.xor(second);
+                    else second.xor(first);
+                    break;
+                    
+                case "FLIP":
+                    if (firstArg == 1) first.flip(secondArg);
+                    else second.flip(secondArg);
+                    break;
+
+                case "SET":
+                    if (firstArg == 1) first.set(secondArg);
+                    else second.set(secondArg);
+                    break;
+                    
+                default:
+                    break;
             }
-            else if (nextCommand.equals("OR")) {
-                if (firstArg == 1) {
-                    first.or(second);
-                } 
-                else {
-                    second.or(first);
-                }
-            }
-            else if (nextCommand.equals("XOR")) {
-                if (firstArg == 1) {
-                    first.xor(second);
-                } 
-                else {
-                    second.xor(first);
-                }
-            }
-            else if (nextCommand.equals("FLIP")) {
-                if (firstArg == 1) {
-                    first.flip(secondArg);
-                } 
-                else {
-                    second.flip(secondArg);
-                }
-            }
-            else if (nextCommand.equals("SET")) {
-                if (firstArg == 1) {
-                    first.set(secondArg);
-                } 
-                else {
-                    second.set(secondArg);
-                }
-            }
-            printBitSetCounts(first, second, setSize);
+
+            // print count of all flipped bits instead of only given range
+            System.out.println(first.cardinality() + " " + second.cardinality());
         }
         
+        // close scanner
         s.close();
-    }
-
-    
-    private static void printBitSetCounts(BitSet a, BitSet b, int setSize) {
-        int aCount = 0;
-        int bCount = 0;
-        for (int i = 0; i < setSize; i++) {
-            if (a.get(i)) aCount++;
-            if (b.get(i)) bCount++;
-        }
-        System.out.println(String.valueOf(aCount) + " " + String.valueOf(bCount));
     }
 }
