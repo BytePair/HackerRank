66dbfc15c507bd3bf2d006dd4a8434ccaeada6bd
Gerald Holmes <bytepair@gmail.com>
2017-11-19 22:23:48 -0500

    Start Python Challenges

---
+++ Python/Introduction/ifelse.py
@@ -0,0 +1,15 @@
+# If  is odd, print Weird
+# If  is even and in the inclusive range of  to , print Not Weird
+# If  is even and in the inclusive range of  to , print Weird
+# If  is even and greater than , print Not Weird
+
+if __name__ == '__main__':
+    n = int(input())
+    if n % 2 == 1:
+        print("Weird")
+    elif 2 <= n <= 5:
+        print("Not Weird")
+    elif 6 <= n <= 20:
+        print("Weird")
+    else:
+        print("Not Weird")
