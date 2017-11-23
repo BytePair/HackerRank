dd6b08b88776252158148a9bb437e95a22eb619c
Gerald Holmes <bytepair@gmail.com>
2017-11-22 20:46:39 -0500

    Start python strings

---
+++ Python/Strings/SwapCase.py
@@ -0,0 +1,18 @@
+def swap_case(s):
+    swapped_s = ""
+    for char in s:
+        if char.isalpha():
+            if char.islower():
+                swapped_s += char.upper()
+            if char.isupper():
+                swapped_s += char.lower()
+        else:
+            swapped_s += char
+    return swapped_s
+
+
+if __name__ == "__main__":
+
+    s = input()
+    result = swap_case(s)
+    print(result)
